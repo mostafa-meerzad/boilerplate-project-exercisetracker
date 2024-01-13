99abb7ab964034f13420730613b63720e7def6f2
mostafa <mostafameerzad@gmail.com>
2024-01-13 22:51:11 +0430

    Connect the app to mongoDB Atlas

--- startup/db.js
+++ startup/db.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
-// "mongodb+srv://mostafa:mEGKue60u2QV8KCr@cluster0.nkbvvbe.mongodb.net/userExercises"
-// module.exports = function () {
 mongoose
-  .connect("mongodb://localhost:27017/userExercises")
+  .connect(
+    `mongodb+srv://mostafa:${process.env.DBKEY}.nkbvvbe.mongodb.net/userExercises`
+  )
   .then(() => {
     console.log("connected to db");
   })
   .catch((e) => {
     throw new Error(e);
   });
-// };
